20b82e12b421dd073c8254c9d437201b9be64508
Björn <bo@kbct.de>
2017-01-15 20:08:50 +0100

    Prepare for personality test

--- js/quiz.js
+++ js/quiz.js
@@ -1,73 +1,49 @@
-/**
- * kommentare oben fühlen sich richtig an.
- */
-
 var questions =
 [
-{"question":"Ab wann ist man in Deutschland volljährig?","answers":["Ab 18 Jahren","Ab 16 Jahren","Ab 21 Jahren","Ab 19 Jahren"]},
-{"question":"Ab wie vielen Jahren darf man in Deutschland Auto fahren?","answers":["Ab 17/18 Jahren","Ab 15/16 Jahren","Ab 20/21 Jahren","Ab 19/20 Jahren"]},
-{"question":"Wie heißt die deutsche Verfassung?","answers":["Grundgesetz","Volksgesetz","Deutsches Gesetz","Bundesgesetz"]},
-{"question":"Deutschland ist …","answers":["Eine Demokratie","Ein sozialistischer Staat","Eine Diktatur","Eine Monarchie"]},
-{"question":"Was hat jedes deutsche Bundesland?","answers":["Eine eigene Regierung","Eine eigene Armee","Eine eigene Währung","Eine eigene/eigenen Außenminister/in"]},
-{"question":"In der DDR lebten vor allem Migranten aus …","answers":["Polen, Vietnam, Mosambik","Frankreich, Rumänien, Somalia","Chile, Ungarn, Simbabwe","Nordkorea, Mexiko, Ägypten"]},
-{"question":"Wer oder was ist „One Direction“?","answers":["Eine Boyband","Eine Richtung","Keine Ahnung","Eine englische Verkehrsregel"]},
-{"question":"Was ist Spotify?","answers":["Ein Musikstreamingportal","Ein Telefonanbieter","Eine neue Sportart","Eine Stadt"]},
-{"question":"Was ist der Superbowl?","answers":["Das Finale der American-Football-Liga in den USA","Ein Fest","Ein Trinkspiel","War das nicht irgendwas mit Football?"]},
-{"question":"Was ist die Bundesliga?","answers":["Die höchste deutsche Spielklasse im Fußball","Eine Liga auf Bundesebene","Irgendwas mit Fußball","Ein Gesetz"]},
-{"question":"Wann ist die nächste Fussball-WM?","answers":["Im Jahr 2018","Im Jahr 2020","Im Jahr 2033","Im Jahr 2017"]},
-{"question":"Wann wurde „One Direction“ gegründet?","answers":["Im Jahr 2010 bei „X-Factor UK“","Im Jahr 2000 bei „X-Factor UK“","Im Jahr 2008 bei „The Voice“","Im Jahr 2010 bei „The Voice“"]},
-{"question":"Mit welchem Lied war Adele auf Platz 1 der Charts 2016?","answers":["Mit „Hello“","Mit „Rolling in the Deep“","Mit „Someone Like You“","Mit „When We Were Young“"]},
-{"question":"Wer war der erste deutsche Bundeskanzler?","answers":["Konrad Adenauer","Angela Merkel","Jonathan Meier","Peter Maier"]},
-{"question":"Welche dieser Personen war nie Präsident des deutschen Bundestags?","answers":["Helmut Schmidt","Rita Süssmuth","Wolfgang Thierse","Angela Merkel"]},
-{"question":"Wann starb Michael Jackson?","answers":["Im Jahr 2009","Im Jahr 2008","Im Jahr 2010","Im Jahr 2011"]},
-{"question":"Wie heißt der aktuelle Finanzminister in Deutschland?","answers":["Wolfgang Schäuble","Richard Jungenmeier","Peter Altmaier","Franz Josef Jung"]},
-{"question":"Was ist „Orange is the New Black“?","answers":["Eine Serie","Ein Film","Ein Computerspiel","Eine Kleidermarke"]},
-{"question":"In welcher Höhe hängt der Korb beim Basketball?","answers":["3,05 m","4,00 m","2,80 m","2,30 m"]},
-{"question":"Wann war der Urknall ungefähr?","answers":["13,8 Mrd. Jahre","1 Mrd. Jahre","12,7 Mrd. Jahre","100 Mio. Jahre"]},
-{"question":"Wie viele Länder gibt es insgesamt auf der Erde?","answers":["194","140","98","255"]},
-{"question":"Wie viele Einwohner hat Deutschland?","answers":["80,62 Mio.","22,8 Mio.","111 Mio.","66 Mio."]},
-{"question":"Welche Stadt ist die zweitgrößte Stadt Deutschlands (nach Einwohnern)?","answers":["Hamburg","Berlin","München","Köln"]},
-{"question":"Wie alt wurde Thomas Mann?","answers":["80 Jahre","90 Jahre","55 Jahre","79 Jahre"]},
-{"question":"Wie hießen Thomas Manns Brüder?","answers":["Heinrich Mann und Viktor Mann","Julius Mann und Günter Mann","Jakob Mann und Thomas Mann","Heinrich Mann und Lucas Mann"]},
-{"question":"Wie viele Geschwister hatte Thomas Mann?","answers":["4","2","3","1"]},
-{"question":"Wie hießen Thomas Manns Eltern?","answers":["Thomas Johann Heinrich und Julia","Heinrich und Anne","Johann und Julia","Manfred und Manuela"]},
-{"question":"In welchem Jahr zog Thomas Mann aus Lübeck weg?","answers":["1894","1905","1910","1890"]},
-{"question":"Was heißt „BRD“?","answers":["Bundesrepublik Deutschland","Bayrisches Radio-Denkmal","Blackrock Depths","Biologischer Rest-Dünger"]},
-{"question":"Wann startete die Erfolgsserie „The Walking Dead“?","answers":["2010","2008","2009","2007"]},
-{"question":"Wer ist Thomas Gottschalk?","answers":["Moderator","Schauspieler","Sänger","Filmproduzent"]},
-{"question":"Wie viele Spieler sind in der Fußballnationalmannschaft?","answers":["11","20","14","18"]},
-{"question":"Wie oft gewann Deutschland die Fußball-WM?","answers":["Viermal","Fünfmal","Dreimal","Einmal"]},
-{"question":"Wer ist die Synchronstimme von Homer Simpson?","answers":["Dan Castellaneta","Norbert Gastell","Walter Matthau","Ernst Jacobi"]},
-{"question":"Welche ist die meistverbreitete Religion in Deutschland?","answers":["Christentum","Judentum","Islam","Buddhismus"]},
-{"question":"Welche dieser Automarken ist keine deutsche Automarke?","answers":["Ford","Smart","Porsche","BMW"]},
-{"question":"Wann wurde die Berliner Mauer gebaut?","answers":["Im August 1961","Im August 1962","Im September 1961","Im November 1964"]},
-{"question":"Wann ist die Berliner Mauer gefallen?","answers":["Im November 1989","Im November 1990","Im Dezember 1989","Im Dezember 1990"]},
-{"question":"Was war Thomas Mann von Beruf?","answers":["Schriftsteller","Fotograf","Regisseur","Schauspieler"]},
-{"question":"Welche Farben hat der Regenbogen (in richtiger Reihenfolge)?","answers":["Rot, orange, gelb, grün, blau, lila","Rot, blau, gelb, grün, orange, lila","Orange, blau, lila, grün, gelb, rot","Grün, blau, lila, orange, gelb, rot"]},
-{"question":"In welchem Jahr wurde Angela Merkel geboren?","answers":["1954","1953","1955","1956"]},
-{"question":"Wie viele Bundesländer hat Deutschland?","answers":["16","15","17","14"]},
-{"question":"Wie starb John F. Kennedy?","answers":["Durch einen Kopfschuss","Durch Suizid","Eines natürlichen Todes","Durch Gift"]},
-{"question":"Wie nennt man das beliebteste schottische Musikinstrument?","answers":["Dudelsack","Mundharmonika","Flöte","Gitarre"]},
-{"question":"Welcher Komponist schrieb die Musik für das Ballett „Schwanensee“?","answers":["Tschaikowski","Chopin","Schostakowitsch","Rachmaninow"]},
-{"question":"In welchem Jahr veröffentlichte Martin Luther die 95 Thesen?","answers":["1517","1614","1714","1417"]},
-{"question":"Wann wurde der Nobelpreis zum ersten Mal vergeben?","answers":["1901","1898","1903","1896"]},
-{"question":"Wie stellt man fest, ob eine Zahl durch 5 teilbar ist?","answers":["Die letzte Ziffer muss eine 5 oder 0 sein.","Die letzte Ziffer muss eine 5 sein.","Die letzte Ziffer muss eine 2 sein.","Die Quersumme der Zahl muss durch 5 teilbar sein."]},
-{"question":"Was feiern Christen am Ostersonntag?","answers":["Jesu Auferstehung","Jesu Geburt","Die Segnung der Speisen","Die Empfängnis des Heiligen Geistes"]},
-{"question":"Was ist ein Ozelot?","answers":["Eine Wildkatze","Ein Krebs","Ein Gerät zur Messung der Luftfeuchtigkeit","Ein Gerät zur Tiefenmessung im Wasser"]}
+  {
+    "question": "Ab wann ist man Mann?",
+    "answers": [
+      "1 foo",
+      "2 bar",
+      "3 baz",
+      "4 boo",
+      "5 asd",
+      "6 pop"
+    ]
+  },
+  {
+    "question": "Mann 2 oder 3?",
+    "answers": [
+      "1 foo",
+      "2 bar",
+      "3 baz",
+      "4 boo",
+      "5 asd",
+      "6 pop"
+    ]
+  },
 ];
 
-var scoreSubTitles = [
-{min: 0, text: "Hast Du schonmal was von Wikipedia gehört?"},
-{min: 1, text: "Das geht noch besser!"},
-{min: 4, text: "Du hast Dir Mühe gegeben und landest im Mittelfeld."},
-{min: 7, text: "Das hast Du gut gemacht!"},
-{min: 9, text: "Super! Klasse! Toll gemacht! Sehr schön."},
-];
+var scoreSubTitles = {
+  "Mann1": "Hast Du schonmal was von Wikipedia gehört?",
+  "Mann2": "Das geht noch besser!",
+  "Mann3": "Du hast Dir Mühe gegeben und landest im Mittelfeld.",
+  "Mann4": "Das hast Du gut gemacht!",
+  "Mann5": "Super! Klasse! Toll gemacht! Sehr schön.",
+  "Mann6": "Top! Klasse! Toll gemacht! Sehr schön.",
+};
 
 var state = {
 	questionIndex: -1,
 	questionsDone: [], // indexes
-	answersRight: 0,
+	answers: [
+    {name: "Mann1", score: 0},
+    {name: "Mann2", score: 0},
+    {name: "Mann3", score: 0},
+    {name: "Mann4", score: 0},
+    {name: "Mann5", score: 0},
+    {name: "Mann6", score: 0},
+  ],
 };
 
 var elements = {
@@ -90,6 +66,14 @@ function render() {
 	elements.header.classList[done ? 'add' : 'remove']('hidden');
 
 	if (done) {
+    renderScore();
+	} else {
+    renderQuiz();
+	}
+}
+
+// TODO
+function renderScore() {
 		saveScore();
 		elements.scoreTitle.textContent = state.answersRight + '/10 richtig beantwortet!';
 		var subtitle = '';
@@ -99,19 +83,23 @@ function render() {
 		});
 		elements.scoreText.textContent = subtitle;
 		elements.scorePercent.textContent = getPercentage() + '% haben genau das gleiche Ergebnis erzielt wie Du!';
-	} else {
+}
+
+function renderQuiz() {
 		var count = state.questionsDone.length + 1;
 		if (count < 10)
 			count = '0' + count;
 		elements.counter.textContent = count;
+
 		var q = questions[state.questionIndex];
 		elements.question.textContent = q.question;
-		var answers = q.answers.slice(0);
-		answers.sort(function() { return 0.5 - Math.random();  });
-		answers.forEach(function (a, i) {
-			elements.answers[i].textContent = a;
+
+    var answerElements = elements.answers.slice(0);
+		answerElements.sort(function() { return 0.5 - Math.random();  });
+		answerElements.forEach(function (el, i) {
+      el.textContent = q.answers[i];
+      el.dataset.answerIndex = i;
 		});
-	}
 }
 
 function saveScore() {
@@ -134,42 +122,30 @@ function getPercentage() {
 }
 
 function newQuestion() {
-	// my god
 	do {
 		state.questionIndex = Math.floor(Math.random() * questions.length);
 	} while (state.questionsDone.indexOf(state.questionIndex) !== -1)
 }
 
 function selectAnswer(event) {
-	var right = questions[state.questionIndex].answers[0] == event.target.textContent;
-	if (right)
-		state.answersRight++;
+  var answerIndex = event.target.dataset.answerIndex;
+  state.answers[answerIndex].score++;
+
 	state.questionsDone.push(state.questionIndex);
 
 	var done = function() {
 		setTimeout(function() {
-			addEvents();
 			newQuestion();
+			addEvents();
 			render();
 		}, 400);
 	};
 
 	removeEvents();
-	event.target.classList.add(right ? 'right' : 'wrong');
+	event.target.classList.add('clicked');
 	setTimeout(function() {
-		event.target.classList.remove(right ? 'right' : 'wrong');
-		if (right) {
-			done();
-		} else {
-			var rightEl = elements.answers.find(function(el) {
-				return el.textContent === questions[state.questionIndex].answers[0];
-			});
-			rightEl.classList.add('right');
-			setTimeout(function() {
-				rightEl.classList.remove('right');
-				done();
-			}, 400);
-		}
+		event.target.classList.remove('clicked');
+    done();
 	}, 400);
 }
 
@@ -186,8 +162,8 @@ function removeEvents() {
 }
 
 function init() {
-	addEvents();
 	newQuestion();
+	addEvents();
 	render();
 }
 
